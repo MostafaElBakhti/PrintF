4918188e48db422c3583d961b8d7c1e329485b75
Mostafa El Bakhti <mel-bakh@c2r7p9.1337.ma>
2025-12-27 18:25:48 +0100

    fix the errors

--- ft_printf.c
+++ ft_printf.c
@@ -6,26 +6,26 @@
 /*   By: mel-bakh <mel-bakh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/29 15:02:47 by mel-bakh          #+#    #+#             */
-/*   Updated: 2025/12/22 18:30:29 by mel-bakh         ###   ########.fr       */
+/*   Updated: 2025/12/27 18:21:25 by mel-bakh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_format(va_list *args, char c)
+int	ft_format(va_list args, char c)
 {
 	if (c == 's')
-		return (ft_putstr(va_arg(*args, char *)));
+		return (ft_putstr(va_arg(args, char *)));
 	else if (c == 'c')
-		return (ft_putchar(va_arg(*args, int)));
+		return (ft_putchar(va_arg(args, int)));
 	else if (c == 'd' || c == 'i')
-		return (ft_putnbr(va_arg(*args, int)));
+		return (ft_putnbr(va_arg(args, int)));
 	else if (c == 'u')
-		return (ft_putunsigned(va_arg(*args, unsigned int)));
+		return (ft_putunsigned(va_arg(args, unsigned int)));
 	else if (c == 'x' || c == 'X')
-		return (ft_puthex(va_arg(*args, unsigned int), c));
+		return (ft_puthex(va_arg(args, unsigned int), c));
 	else if (c == 'p')
-		return (ft_putptr(va_arg(*args, unsigned long long)));
+		return (ft_putptr(va_arg(args, unsigned long long)));
 	else if (c == '%')
 		return (ft_putchar('%'));
 	else
@@ -43,7 +43,9 @@ int	ft_printf(const char *format, ...)
 	int		tmp;
 	va_list	args;
 
-	if (!format)
+	// if (!format)
+	// 	return (-1);
+	if (write(1, "", 0) == -1)
 		return (-1);
 	va_start(args, format);
 	i = 0;
@@ -52,7 +54,7 @@ int	ft_printf(const char *format, ...)
 	{
 		tmp = 0;
 		if (format[i] == '%' && format[i + 1])
-			tmp = ft_format(&args, format[++i]);
+			tmp = ft_format(args, format[++i]);
 		else
 			tmp = ft_putchar(format[i]);
 		if (tmp == -1)
@@ -63,3 +65,7 @@ int	ft_printf(const char *format, ...)
 	va_end(args);
 	return (count);
 }
+// int main()
+// {
+// 	ft_printf("hsdjkh%");
+// }
