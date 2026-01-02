580be10a660c32827bceef7da0a08ad7cf5783ee
mostafa <mustmustapha67@gmail.com>
2026-01-02 14:48:53 +0100

    fix errors

--- ft_putptr.c
+++ ft_putptr.c
@@ -28,15 +28,12 @@ int	ft_putptr(void *add)
 	unsigned long long	to_num;
 	int					count;
 
-	to_num = (unsigned long long)add;
-	count = 0;
 	if (!add)
 		return (ft_putstr("0x0"));
-	if (to_num)
-	{
-		count += ft_putchar('0');
-		count += ft_putchar('x');
-	}
+	to_num = (unsigned long long)add;
+	count = 0;
+	count += ft_putchar('0');
+	count += ft_putchar('x');
 	count += ft_print_ptrhex(to_num);
 	return (count);
 }
